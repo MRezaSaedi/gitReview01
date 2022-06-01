1904a4934488cdc4fd3de12a397a1e20d5ce147d
MRezaSaedi <mreza.saedi@gmail.com>
2022-05-31 18:52:27 -0700

    2nd commit

--- src/gitReview/getreview01.java
+++ src/gitReview/getreview01.java
@@ -5,6 +5,7 @@ public class getreview01{
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Github day");
+		System.out.println("last day");
 
 	}
 
